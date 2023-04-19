8a1026935b378618fc88e60f833a5565049528c3
Savvas <savvascyp@hotmail.com>
2023-04-19 17:19:25 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker run --name my-sass -it --rm -v $PWD:/web-mock -w /web-mock -d node:17 ./generate-css.sh
+docker run --name my-nginx -v $PWD:/usr/share/nginx/html:ro -p 80:80 -d nginx:1.23.3-alpine
