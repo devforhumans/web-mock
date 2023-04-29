722a9f9f311e4813254c3ec5e91a19304ec32e4f
Savvas <savvascyp@hotmail.com>
2023-04-29 18:50:20 +0100

    Site name as link

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-docker run --name my-sass -it --rm -v $PWD:/web-mock -w /web-mock -d node:17 ./generate-css.sh
+docker run -it --rm -v $PWD:/web-mock -w /web-mock -d node:17 ./generate-css.sh
 docker run --name my-nginx -v $PWD:/usr/share/nginx/html:ro -p 80:80 -d nginx:1.23.3-alpine
+docker run -it --rm -v $PWD:/web-mock -w /web-mock node:17 bash
